62b5f0f4322f821765dd182f097b5948399a851d
Rakshan Hegde <hegde.rakshan@gmail.com>
2020-10-01 06:47:40 +0000

    Done.

--- calculator.rb
+++ calculator.rb
@@ -1,1 +1,6 @@
-# Add your variables here
+first_number=10
+second_number=8
+sum=first_number+second_number
+difference=first_number-second_number
+product=first_number*second_number
+quotient=first_number/second_number
